d1c6a89d91589c2b5f1cbc207fb639a75679279d
Luana-L <luana.liao.6@gmail.com>
2025-04-22 19:26:24 -0400

    added third lesson page and refactored slider styling

--- static/js/learn.js
+++ static/js/learn.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+
+        var $slider = $('#oz-slider-learn');
+        var $pourBtn = $('#pour-button-learn');
+        var $continueBtn = $('#continue-button');
+
+        if ($slider.length && $pourBtn.length && $continueBtn.length) {
+                var correctIndex = parseInt($slider.data('correct'));
+
+                // Initially disable POUR and CONTINUE
+                $pourBtn.prop('disabled', true);
+                $continueBtn.addClass('disabled').attr('aria-disabled', 'true');
+
+                $slider.on('input change', function () {
+                        var selectedIndex = parseInt($slider.val());
+                        $pourBtn.prop('disabled', selectedIndex !== correctIndex);
+                });
+
+                $pourBtn.on('click', function () {
+                        $slider.prop('disabled', true); // disable dragging
+                        $continueBtn.removeClass('disabled').removeAttr('aria-disabled');
+                });
+        }
+              
         $('.nav-link').on('click', function (e) {
                 e.preventDefault(); // Prevent default anchor behavior
     
